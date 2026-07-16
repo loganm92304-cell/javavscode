7e97f7ce0a25939a04c8eb29c0ecd29f2727e159
Logan Marsh <loganm92304@gmail.com>
2026-07-16 10:26:52 -0230

    Question 2 Code

---
+++ QAP 3/src/q2/Demo.java
@@ -0,0 +1,11 @@
+public class Demo {
+    public static void main(String[] args) {
+        Point p1 = new Point(1.5f, 2.5f);
+        System.out.println("Point: " + p1);
+
+        MovablePoint mp1 = new MovablePoint(3.0f, 4.0f, 0.5f, 1.5f);
+        System.out.println("Before Move: " + mp1);
+        mp1.move();
+        System.out.println("After Move:  " + mp1);
+    }
+}
