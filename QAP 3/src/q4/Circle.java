0cc98b1598a918a92698d5e4c71c599190836183
Logan Marsh <loganm92304@gmail.com>
2026-07-16 10:30:03 -0230

    Question 4 Code

---
+++ QAP 3/src/q4/Circle.java
@@ -0,0 +1,14 @@
+/**
+ * Represents a Circle (a special case of Ellipse where a = b = radius).
+ */
+public class Circle extends Ellipse {
+    
+    public Circle(String name, double radius) {
+        super(name, radius, radius);
+    }
+
+    @Override
+    public void scale(double factor) {
+        super.scale(factor);
+    }
+}
---
+++ QAP 3/src/q4/EquilateralTriangle.java
@@ -0,0 +1,14 @@
+/**
+ * Represents an Equilateral Triangle.
+ */
+public class EquilateralTriangle extends Triangle {
+
+    public EquilateralTriangle(String name, double side) {
+        super(name, side, side, side);
+    }
+
+    @Override
+    public void scale(double factor) {
+        super.scale(factor);
+    }
+}
